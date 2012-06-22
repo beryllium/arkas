0bb82c105cf6474492fb4c3dc5e9fa3cd014dea3
Kevin Boyd <kevinb@beryllium.ca>
2012-06-22 11:49:28 -0700

    Updating some docblocks, adjusting some formatting, adding phpstorm to gitignore

--- src/Arkas/Grok/DefaultFileGrok.php
+++ src/Arkas/Grok/DefaultFileGrok.php
@@ -1,36 +1,45 @@
 <?php
 namespace Arkas\Grok;
 
+/**
+ * Basic keyword searching using strpos().
+ */
 class DefaultFileGrok implements GrokInterface
 {
-  public $filename;
+    public $filename;
 
-  public function file( $filename )
-  {
-    $this->setData( $filename );
-  }
-
-  public function setData( $data )
-  {
-    $this->filename = $data;
-  }
-
-  public function grok( $keyword )
-  {
-    if ( !file_exists( $this->filename ) ) return false;
-
-    //Open the file and iterate through, matching keywords to the provided one
-    $data = file( $this->filename );
-    $results = array();
-
-    foreach( $data as $key=>$value )
+    public function file($filename)
     {
-      if ( false !== strpos( $value, $keyword ) )
-      {
-        $results[] = new GrokResult( $key, $value, $keyword, $this->filename );
-      }
+        $this->setData($filename);
     }
 
-    return $results;
-  }
+    public function setData($data)
+    {
+        $this->filename = $data;
+    }
+
+    /**
+     * Run the search on the current filename using a simple strpos()
+     *
+     * @param $keyword The needle
+     * @return array|bool An array of GrokResult objects
+     */
+    public function grok($keyword)
+    {
+        if (!file_exists($this->filename)) return false;
+
+        //Open the file and iterate through, matching keywords to the provided one
+        $data = file($this->filename);
+        $results = array();
+
+        foreach ($data as $key => $value)
+        {
+            if (false !== strpos($value, $keyword))
+            {
+                $results[] = new GrokResult($key, $value, $keyword, $this->filename);
+            }
+        }
+
+        return $results;
+    }
 }
--- src/Arkas/Grok/GrokResult.php
+++ src/Arkas/Grok/GrokResult.php
@@ -3,25 +3,37 @@ namespace Arkas\Grok;
 
 class GrokResult
 {
-  public $line;
-  public $data;
-  public $keyword;
-  public $filename;
+    public $line;
+    public $data;
+    public $keyword;
+    public $filename;
 
-  public function __construct( $line, $data, $keyword, $filename )
-  {
-    $this->line = $line;
-    $this->data = $data;
-    $this->keyword = $keyword;
-    $this->filename = $filename;
-  }
+    /**
+     * @param $line Line number of the match
+     * @param $data Raw text of the line
+     * @param $keyword The keyword that was matched
+     * @param $filename The filename of the matching file
+     */
+    public function __construct($line, $data, $keyword, $filename)
+    {
+        $this->line = $line;
+        $this->data = $data;
+        $this->keyword = $keyword;
+        $this->filename = $filename;
+    }
 
-  public function output( $output, $color = 'info' )
-  {
-    $open_color = '<' . $color . '>';
-    $close_color = '</' . $color . '>';
+    /**
+     * Output the result
+     *
+     * @param $output An output interface (requires ->writeln() method)
+     * @param string $color Optional colour for symfony2 console highlighting
+     */
+    public function output($output, $color = 'info')
+    {
+        $open_color = '<' . $color . '>';
+        $close_color = '</' . $color . '>';
 
-    $data = str_replace( $this->keyword, $open_color . $this->keyword . $close_color, $this->data );
-    $output->writeln( $this->filename . ':' . $this->line . ":\t" . rtrim($data) );
-  }
+        $data = str_replace($this->keyword, $open_color . $this->keyword . $close_color, $this->data);
+        $output->writeln($this->filename . ':' . $this->line . ":\t" . rtrim($data));
+    }
 }
